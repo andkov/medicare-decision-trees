2148c524f40b543dea146a2529685e9c7bdb0c49
Andriy V. Koval <koval.andrey@gmail.com>
2019-01-25 15:48:13 -0500

    renamed long varnames

--- manipulation/0-greeter.R
+++ manipulation/0-greeter.R
@@ -46,6 +46,18 @@ names(ds) <- tolower(names(ds))
 # readr::write_csv(as.data.frame(names(ds)), "./data-unshared/derived/rawnames.csv")
 ds %>% dplyr::glimpse(50)  
 
+# renames some of the variables 
+
+ds <- ds %>% 
+  dplyr::rename_(
+    "average_hcc_risk_score"  = "average_hcc_risk_score_of_beneficiaries"
+    ,"non_metropolitan_area"  = "non_metropolitan_area_or_missing_9_counties_missing"
+    ,"mean_age_benef_county"  = "medicare_ffs_benef_average_age_fee_for_service_2014"
+    ,"mean_hcc_benef_county"  = "medicare_ffs_bene_avg_hcc_score__fee_for_service_2014"
+    ,"medicare_cost"          = "standardized_risk_adjusted_per_capita_medicare_costs"
+    ,"total_medicare_payment" = "total_medicare_standardized_payment_amount"
+  )
+
 # ---- group-variables ---------------------------------------
 practice <- c(
  "female" # Gender of the Physical Therapist                                               
@@ -58,23 +70,28 @@ practice <- c(
  ,"physical_agent_pct" # Proportion of physical agents 
  ,"proxy_for_n_of_new_patients"  # Number of new patients                        
  ,"average_age_of_beneficiaries" # Average age of beneficiaries                        
- ,"average_hcc_risk_score_of_beneficiaries" # Average HCC risk score of beneficiaries              
+ # ,"average_hcc_risk_score_of_beneficiaries" # Average HCC risk score of beneficiaries              
+ ,"average_hcc_risk_score"                    # Average HCC risk score of beneficiaries              
  
  ,"large_metro_area"                                     
  ,"mid_sized_metro_area"                                 
  ,"small_metro_area"                                     
- ,"non_metropolitan_area_or_missing_9_counties_missing"  
+ # ,"non_metropolitan_area_or_missing_9_counties_missing"  
+ ,"non_metropolitan_area"  
  )
 
 market <- c(
  "pcp_per_10k_pop_14" # Primary care physicians per 10,000 population, county level                                   
 ,"pt_per_10k_pop_09" # PTs per 10,000 population (2009), county level                                   
-,"medicare_ffs_benef_average_age_fee_for_service_2014" # Average age of beneficiaries, county level 
+# ,"medicare_ffs_benef_average_age_fee_for_service_2014" # Average age of beneficiaries, county level 
+,"mean_age_benef_county"                               # Average age of beneficiaries, county level 
 ,"pct_mdcr_ffs_benef_female_14" # Percent of female beneficiaries, county level                         
-,"medicare_ffs_bene_avg_hcc_score__fee_for_service_2014" # Average HCC risk score of beneficiaries, county level
+# ,"medicare_ffs_bene_avg_hcc_score__fee_for_service_2014" # Average HCC risk score of beneficiaries, county level
+,"mean_hcc_benef_county"                                 # Average HCC risk score of beneficiaries, county level
 ,"pct_mdcr_benef_elig_medcaid_14" # Percent of beneficiaries eligible for Medicaid, county level                      
 ,"pct_mdcr_ff_benef_pop_14" # Beneficiaries as a share of total population, county level                           
- ,"standardized_risk_adjusted_per_capita_medicare_costs" # Standardized Risk-Adjusted Per Capita Medicare Costs, county level 
+ # ,"standardized_risk_adjusted_per_capita_medicare_costs" # Standardized Risk-Adjusted Per Capita Medicare Costs, county level 
+ ,"medicare_cost"                                        # Standardized Risk-Adjusted Per Capita Medicare Costs, county level 
 ,"median_household_income__2014" # Median Household Income, county level                        
 ,"pct_65older_in_deep_poverty_14" # Percent of persons 65 or older in deep poverty, county level                       
 ,"pt_bene_ratio"  # PTs per 10,000 beneficiaries, county level                                      
@@ -82,7 +99,8 @@ market <- c(
 
 outcome <- c(
  "number_of_services"   # Number of services performed                                
-,"total_medicare_standardized_payment_amount" # Total Medicare standardized payment amount   
+# ,"total_medicare_standardized_payment_amount" # Total Medicare standardized payment amount   
+,"total_medicare_payment"                       # Total Medicare standardized payment amount   
 )
 
 unaccounted <- c("therapeutic_pct")
